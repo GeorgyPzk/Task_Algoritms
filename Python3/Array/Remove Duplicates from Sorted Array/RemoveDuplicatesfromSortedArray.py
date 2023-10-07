3c23721157b1664821ff71bfd6524c465994acb9
georgy <georgyVSwork@yandex.ru>
2023-10-07 15:51:53 +0200

    Add stucture and remove dublicats

---
+++ Python3/Array/Remove Duplicates from Sorted Array/RemoveDuplicatesfromSortedArray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        k = 1  # Initialize the unique count to 1 since the first element is always unique
+        n = len(nums)
+
+        for i in range(1, n):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+
+
+
